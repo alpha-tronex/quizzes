ee2be3ab8a89b31dcac1256b9f5e5c157430888e
alpha-tronex <alphatronex@yahoo.com>
2026-01-17 17:46:32 -0500

    Use npm bin to locate ng executable

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ echo "Installing root dependencies with legacy peer deps..."
 npm ci --legacy-peer-deps || npm install --legacy-peer-deps
 
 echo "Building Angular app..."
-export PATH="$PWD/node_modules/.bin:$PATH"
-ng build --configuration production
+$(npm bin)/ng build --configuration production
 
 echo "Installing server dependencies..."
 cd server
