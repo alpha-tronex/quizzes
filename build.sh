da4c8d43419cb9c9ae4ff504d2648eef8570d2b6
alpha-tronex <alphatronex@yahoo.com>
2026-01-17 17:43:35 -0500

    Add node_modules/.bin to PATH for build

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ echo "Installing root dependencies with legacy peer deps..."
 npm ci --legacy-peer-deps || npm install --legacy-peer-deps
 
 echo "Building Angular app..."
-node node_modules/@angular/cli/bin/ng.js build --configuration production
+export PATH="$PWD/node_modules/.bin:$PATH"
+ng build --configuration production
 
 echo "Installing server dependencies..."
 cd server
