c19ceb905a89a71f2ea99a408fe02ed1cdb9b708
alpha-tronex <alphatronex@yahoo.com>
2026-01-19 16:09:23 -0500

    fixed errors before upgrade

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AdminRoutingModule } from './admin-routing.module';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { CreateQuizComponent } from './create-quiz/create-quiz.component';
@@ -16,6 +17,7 @@ import { UploadQuizComponent } from './upload-quiz/upload-quiz.component';
     CommonModule,
     FormsModule,
     AdminRoutingModule
-  ]
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class AdminModule { }
